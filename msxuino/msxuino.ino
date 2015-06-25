872cff0d607bcab8a5bec28d182de2b06bfa2510
Miso Kim <meeso.kim@gmail.com>
2015-06-26 04:09:01 +0900

    Updated for MSX test

--- msxuino/msxuino.ino
+++ msxuino/msxuino.ino
@@ -3,52 +3,67 @@
 #define CS12     5
 #define SLTSL    6
 #define WAITREL  7
-#define INT      8  
+#define WT       8
 #define M1       9
 #define BUSDIR   10
-#define IORQ     11
-#define MERQ     12
-#define WR       13
-#define RD       14
+#define IORQ     14
+#define MERQ     15
+#define WR       16
+#define RD       17
 #define DATAW    PORTF
 #define DATAR    PINF
 #define ADDRL    PINC
 #define ADDRH    PINA
 
-volatile const byte ROMS[] PROGMEM = {
-#include "SKYJAGUA.h" 
+const byte ROMS[] PROGMEM = {
+#include "SKYJAGUA.h"
 };
+unsigned short addr;
+byte b;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(WAITREL, OUTPUT);
-  pinMode(INT, OUTPUT);
   pinMode(BUSDIR, OUTPUT);
+  pinMode(WT, INPUT);
+  pinMode(RD, INPUT);
+  pinMode(WR, INPUT);
   digitalWrite(WAITREL, HIGH);
-  digitalWrite(INT, HIGH);
   digitalWrite(BUSDIR, HIGH);
   Serial.begin(115200);
   Serial.println(F("MSXuino initialized"));
+  addr = 0;
+  b = ROMS[addr];
+  Serial.println(b);
+  DDRF = 0;
+  DDRA = 0;
+  DDRC = 0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int addr;
-  digitalWrite(WAITREL, HIGH);
-  while(digitalRead(SLTSL));
-  if (!digitalRead(MERQ))
+  //  digitalWrite(WAITREL, HIGH);
+  while (!digitalRead(WT));
+  Serial.println(digitalRead(8));
+  if (!digitalRead(RD))
   {
-    if (!digitalRead(RD) & !digitalRead(CS1))
-    {
-      digitalWrite(BUSDIR, LOW);
-      DDRF = 0xff;
-      addr = (ADDRH << 8 - 0x40) + ADDRL;
-      DATAW = ROMS[addr];
-      digitalWrite(WAITREL,LOW);
-      while(!digitalRead(SLTSL));
-      digitalWrite(BUSDIR, HIGH);
-      DDRF = 0;
-      Serial.println(addr);
-    }
+    DDRF = 0xff;
+    addr = (ADDRH) << 8 + ADDRL;
+    b = ROMS[addr];
+    PORTK = b;
+    digitalWrite(BUSDIR, LOW);
+    digitalWrite(WAITREL, LOW);
+    digitalWrite(WAITREL, HIGH);
+    while (!digitalRead(SLTSL));
+    digitalWrite(BUSDIR, HIGH);
+    DDRF = 0;
+    Serial.println(addr);
+    Serial.println(ADDRH);
+    Serial.println(b);
+  }
+  else
+  {
+    digitalWrite(WAITREL, LOW);
+    digitalWrite(WAITREL, HIGH);
   }
 }
