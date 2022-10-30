affcb122914b931764556a26c3cb6a5ccf52d14e
Miso Kim <meeso.kim@gmail.com>
2022-10-30 21:29:23 +0900

    quick modification

--- sdl-wasm/main.c
+++ sdl-wasm/main.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <SDL.h>
+#include <SDL_video.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 #include <time.h>
@@ -17,10 +18,12 @@ SDL_Renderer *renderer = NULL;
 
 SDL_Point velocity = {0, 0};
 SDL_Rect sprite = {0, 0, size, size};
+SDL_Rect rect = {0, 0, 320, 240};
 SDL_Texture *texture = NULL;
+SDL_Texture *pbuf = NULL;
 
 bool init() {
-     if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO|SDL_INIT_TIMER) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO|SDL_INIT_TIMER) < 0) {
         return false;
     }
     if (TTF_Init() < 0)
@@ -32,6 +35,20 @@ bool init() {
     if (window == NULL | renderer == NULL) {
         return false;
     } 
+
+    char *bitmap = malloc(320*240);
+    SDL_Surface *surf = SDL_CreateRGBSurfaceFrom(bitmap, 320, 240, 8, 320, 0, 0, 0, 0);
+    SDL_Color colors[256];
+    int i;
+    for(i = 0; i < 256; i++)
+    {
+        colors[i].r = colors[i].g = colors[i].b = (Uint8)i;
+    }
+    SDL_SetPaletteColors(surf->format->palette, colors, 0, 256);
+    pbuf = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, 
+        SDL_TEXTUREACCESS_STREAMING, 320, 240);
+    // printf("pbuf:%04x\n", pbuf);
+    SDL_SetWindowTitle(window, "SPC-1000 emulator");    
 
     return true;
 }
@@ -45,7 +62,13 @@ void load_textures() {
     texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
 }
-
+void ToggleFullScreen()
+{
+    static int fullscreen = SDL_WINDOW_FULLSCREEN;
+    SDL_SetWindowFullscreen(window, fullscreen);
+    fullscreen = fullscreen ? 0 : SDL_WINDOW_FULLSCREEN;
+    // printf("%d\n", fullscreen);
+}
 void process_event(SDL_Event *event) {
     SDL_Keycode key = event->key.keysym.sym;
     // printf("%d\n", key);
@@ -55,6 +78,9 @@ void process_event(SDL_Event *event) {
         }
         if (key == SDLK_UP || key == SDLK_DOWN) {
             velocity.y = key == SDLK_UP ? -1 : 1;
+        }
+        if (key == SDLK_F11) {
+            ToggleFullScreen();
         }
     }
     if (event->key.type == SDL_KEYUP) {
@@ -84,6 +110,17 @@ void update() {
 }
 
 void draw() {
+    int *buf;
+    int pitch;
+    if (!SDL_LockTexture(pbuf, NULL, (void**)&buf, &pitch))
+    {
+        for (uint32_t i = 0; i < 320 * 240; ++i)
+            buf[i] = rand();
+        // printf("buf:%04x", buf[0]);
+        // Unlock the texture in VRAM to let the GPU know we are done writing to it
+        SDL_UnlockTexture(pbuf);
+        SDL_RenderCopy(renderer, pbuf, NULL, &rect);
+    }
     SDL_RenderCopy(renderer, texture, NULL, &sprite);
 }
 
@@ -108,24 +145,9 @@ void destroy() {
 #ifndef WASM
 void emscripten_set_main_loop(void (*loop)(), int x, int y )
 {
-
-    // Event loop
     while(!quit)
     {    
-        SDL_Event e;
-
-        // Wait indefinitely for the next available event
-        // SDL_PeepEvents(&e, 1,  SDL_PEEKEVENT, SDL_QUIT, SDL_WINDOWEVENT);
-        // // User requests quit
-        // if(e.type == SDL_QUIT)
-        // {
-        //     quit = true;
-        //     printf("quit\n");
-        // }
-        // else
-        {
-            loop();
-        }
+        loop();
     }
 }
 #endif 
