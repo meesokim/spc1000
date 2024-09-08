3be95b7ee8bed3bff299ab02f8d85c97a1c17ca0
Miso Kim <meeso.kim@gmail.com>
2024-09-09 01:33:48 +0900

    ../UGUI ../emu2149 ../jgz80 download_files.py web_mirrioring.py

--- webcopy/download_files.py
+++ webcopy/download_files.py
@@ -1,5 +1,15 @@
-import sys, os, requests, time
+import sys, os, requests, time, threading, urllib3, zipfile
 from urllib.parse import urlparse
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def download(url, fullpath):
+    global filename
+    response = requests.get(url, verify=False)
+    if response.status_code == 200:
+        open(fullpath,'wb').write(response.content)
+    if response.status_code == 404:
+        open(f'{filename}.404','a').write(url+'\n')
+        print(url, 'error', response.status_code)
 
 if __name__=='__main__':
     subpath = ''
@@ -12,23 +22,40 @@ if __name__=='__main__':
         os.exit()
     if os.path.exists(filename):
         files = open(filename,'r').read().split('\n')
+        filename404 = f'{filename}.404'
+        if os.path.exists(filename404):
+            f404 = open(filename404,'r').read().split('\n')
+            files = list(set(files)-set(f404))
         for url in files:
+            if '?' in url:
+                continue
             o = urlparse(url)
             fullpath = f'{os.environ['HOME']}/{o.hostname}{requests.utils.unquote(o.path)}'
             if len(subpath) and not subpath in fullpath:
                 continue
             path = '/'.join(fullpath.split('/')[:-1])
-            if os.path.exists(fullpath):
-                print(fullpath, 'already exists')
-                continue
-            print(fullpath, 'downloading... ')
+            if os.path.exists(fullpath) and os.stat(fullpath).st_size > 0:
+                ret = None
+                ftext = os.path.splitext(fullpath)
+                if ftext[1].lower() == '.zip':
+                    # print(fullpath, 'zipfile check')
+                    try:
+                        the_zip_file = zipfile.ZipFile(fullpath)
+                        ret = the_zip_file.testzip()
+                    except zipfile.BadZipfile as ex:
+                        print(fullpath, 'is not a zipfile?')
+                        import magic
+                        print(magic.from_file(fullpath))
+                    except:
+                        ret = False
+                if ret is None:
+                    # print(fullpath, 'already exists')
+                    continue
+            print(fullpath, 'downloading ...')
             if not os.path.exists(path):
                 os.makedirs(path)
-            response = requests.get(url)
-            if response.status_code == 200:
-                open(fullpath,'wb').write(response.content)
-                time.sleep(5)
-            else:
-                print(url, 'error')
+            task = threading.Thread(target=download, args=(url, fullpath))
+            task.start()
+            time.sleep(2)
     else:
         print(filename, 'not exists')
--- webcopy/web_mirrioring.py
+++ webcopy/web_mirrioring.py
@@ -1,31 +1,81 @@
 import sys
-import requests
+import requests, time
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
+import nest_asyncio
+from urllib.parse import urlparse
+nest_asyncio.apply()
+# from requests_html import AsyncHTMLSession
+from requests_html import HTML, HTMLSession, AsyncHTMLSession
+gurls = []
 def get_links(url, link=''):
-    if url[-1] != '/':
-        url = url + '/' + link
-    else:
-        url = url + link
-    print(url)
+    global gurls
+    if len(link):
+        if url[-4:] != 'html' and url[-3:] != 'htm' and url[-1] != '/' and '?' not in url:
+            url = url + '/' + link
+        else:
+            url = url + link
+    if url in gurls:
+        return []
+    print('get_links:', url)
+    o = urlparse(url)
     fileurls = []
-    response = requests.get(url)
-    if response.status_code == 200:
-        html = response.text
-        soup = BeautifulSoup(html, 'html.parser')    
-        for tag in soup.find_all('a', href=True):
-            link = tag['href']
-            if link[0] >= '0' and link[0] != '?' and link[0] != '/' and link[:4] != 'http':
-                if link[-1] == '/':
-                    # suburls.append(url + '/' + link)
-                    fileurls.extend(get_links(url, link))
+    session = HTMLSession()
+    try:
+        r = session.get(url, verify=False)
+        r.html.arender()
+        if len(o.path) > 1:
+            surl = url.replace(o.path, '')
+        else:
+            surl = url
+        print(1, surl, o.path)
+        print(r.html.links)
+        for link in r.html.links:
+            print('link', link)
+            if (not 'http' in link) and len(link) > 1:
+                if link[0] == '/':
+                    link = surl + link
                 else:
-                    fileurls.append(url+link)
-            # else:
-            #     print(f'{link} removed')
-    if len(fileurls) > 0:
-        print('\n'.join(fileurls))
+                    if url[-1] == '/':
+                        link = url + link
+                    else:
+                        link = url + '/' + link
+                if link[-1] == '/' or link[-4:] == 'html' or link[-3:] == 'htm':
+                    gurls.append(url)
+                    fileurls.extend(get_links(link))
+                elif '?' not in link:
+                    print(link)
+                    fileurls.append(link)
+            elif surl in link and '?' in link:
+                fileurls.extend(get_links(link))
+    except:
+        import traceback
+        traceback.print_exc()
+        time.sleep(5)
+        fileurls.extend(get_links(url))
     return fileurls
+    # response = requests.get(url)
+    # if response.status_code == 200:
+    #     html = response.text
+    #     # print(html)
+    #     soup = BeautifulSoup(html, 'html.parser')
+    #     for tag in soup.find_all('a', href=True):
+    #         link = tag['href']
+    #         if link[0] >= '0' and link[0] != '?' and link[0] != '/' and link[:4] != 'http':
+    #             if link[-1] == '/':
+    #                 # suburls.append(url + '/' + link)
+    #                 fileurls.extend(get_links(url, link))
+    #             else:
+    #                 fileurls.append(url+link)
+    #         # else:
+    #         #     print(f'{link} removed')
+    # else:
+    #     print(response.status_code)
+
+    # if len(fileurls) > 0:
+    #     print('\n'.join(fileurls))
+    # return fileurls
 
 if __name__=='__main__':
     args = sys.argv
